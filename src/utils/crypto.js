28b77de1322822417c95a3c80f85809678b3c049
kams2004 <kamsuperold2004@gmail.com>
2026-01-29 14:44:34 +0100

    update crypto

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,6 +1,6 @@
 import CryptoJS from 'crypto-js';
 
-const SECRET_KEY = process.env.REACT_APP_ENCRYPTION_KEY || 'scholchat-default-key-2024';
+const SECRET_KEY = process.env.REACT_APP_ENCRYPTION_KEY || 'scholchat-secure-key-2024-v1-32b';
 
 export const encryptPassword = (password) => {
   return CryptoJS.AES.encrypt(password, SECRET_KEY).toString();
